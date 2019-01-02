52bcfb36f44159451a7df9c682f19ea86681d2b3
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-01-02 14:16:20 -0800

    LandingPage boilerplate

--- mox-drafting/src/App.js
+++ mox-drafting/src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+
+import LandingPage from "./components/LandingPage/LandingPage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <LandingPage />
       </div>
     );
   }
