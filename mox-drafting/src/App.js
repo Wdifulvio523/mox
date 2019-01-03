e0d7b05b60bb6400d639fdbc940a1973a76fd841
Lambda_School_Loaner_46 <lambda_school_loaner_46@Lambda-School-Loaner-46s-MacBook-Pro.local>
2019-01-02 20:06:30 -0500

    added routes to app in order to connect landing page with Draft page. -wd

--- mox-drafting/src/App.js
+++ mox-drafting/src/App.js
@@ -2,14 +2,19 @@ import React, {Component} from "react";
 import "./App.css";
 import Draft from "./components/Draft";
 import LandingPage from "./components/LandingPage/LandingPage";
+import {Route, Switch} from 'react-router-dom';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingPage />
-        <Draft />
+      <Switch>
+      <Route exact path='/' component={LandingPage}/>
+      <Route exact path='/draft' component={Draft}/>
+      </Switch>
+  
       </div>
     );
   }
