dde34d943bd75c9f165f5dca37546b328adfff9a
Matt Basile <matthewbasile2@gmail.com>
2019-01-03 11:12:45 -0500

    added draft button to player card

--- mox-drafting/src/components/DraftComponent/PlayerCard.js
+++ mox-drafting/src/components/DraftComponent/PlayerCard.js
@@ -1,18 +1,21 @@
 import React from "react";
 import './Draft.css';
 
+
 const PlayerCard = props => {
   const selectedPlayer = props.playerPool.filter(player => {
     return player.playerId === props.selection[0];
   });
   console.log(selectedPlayer);
+  
   return (
-    <div className="player-card">
+    <div className="player-card d-flex ">
       {props.selection.length === 1 ? (
-        <div>
+        <div className="d-flex justify-content-around align-items-end w-100 mb-2">
           <div>{selectedPlayer[0].displayName} </div>
           <div>{selectedPlayer[0].position} </div>
           <div>{selectedPlayer[0].overallRank} </div>
+          <div className="btn btn-sm btn-primary w-25">Draft</div>
         </div>
       ) : null}
     </div>
