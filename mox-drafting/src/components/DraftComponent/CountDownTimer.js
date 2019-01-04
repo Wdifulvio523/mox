752c1e474f7ba6ef743da779af1127483dbe157d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-01-03 18:16:31 -0800

    countdown resets on draft, fixed snake order bug

--- mox-drafting/src/components/DraftComponent/CountDownTimer.js
+++ mox-drafting/src/components/DraftComponent/CountDownTimer.js
@@ -6,10 +6,10 @@ class CountDownTimer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      draftTime: 9,
+      draftTime: 3,
       draftIsOn: false,
-      pickTime: 59,
-      pickIsOn: false
+      pickIsOn: false,
+      pickTime: 10
     };
     this.startTimer = this.startTimer.bind(this);
   }
@@ -21,7 +21,7 @@ class CountDownTimer extends React.Component {
     });
     this.timer = setInterval(() => {
       if (this.state.draftTime === 0) {
-        this.setState({ draftIsOn: false, pickIsOn: true });
+        this.setState({draftIsOn: false, pickIsOn: true});
       } else {
         this.setState({
           draftTime: this.state.draftTime - 1
@@ -32,41 +32,62 @@ class CountDownTimer extends React.Component {
           pickTime: this.state.pickTime - 1
         });
       }
-      // if (this.props.turn !== ) {
-      //   this.setState({pickTime: 59});
-      // }
+      if (this.state.pickTime === 0) {
+        this.setState({
+          pickTime: this.state.pickTime + 10,
+          turn: this.props.turn + 1
+        });
+      }
     }, 1000);
   }
 
-  autoDraftOnClick() {
-    if (this.state.draftTime === 0) {
-      console.log('autodraftonclick2')
-
-      this.props.autoDraft();
+  static getDerivedStateFromProps(props, state) {
+    if (props.turn !== state.turn) {
+      return {
+        turn: props.turn
+      };
     }
-  }
-componentDidMount() {
-    this.autoDraftOnClick();
+    // return null if state/props haven't changed.
+    return null;
   }
 
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevProps.turn !== this.props.turn) {
+      console.log("this.props", this.props.turn);
+      console.log("prevProps", prevProps.turn);
+      console.log("prevState", prevState.turn);
+      console.log("round", this.props.round);
+      this.setState({pickTime: 15});
+    }
+  };
+
+  autoDraftOnClick() {
+    setInterval(() => {
+      if (this.state.draftTime === 0) {
+        this.props.autoDraft();
+      }
+    }, 1500);
+  }
 
   render() {
     return (
       <div className="timer">
         {this.state.pickIsOn === true ? (
-          <h3> Time Remaining: {this.state.pickTime}</h3>
+          <h3>Time Remaining: {this.state.pickTime}</h3>
         ) : (
           <h3>Draft Begins: {this.state.draftTime}</h3>
         )}
-        {this.state.draftTime === 9 ? (
+        {this.state.draftTime === 3 ? (
           <button
             onClick={() => {
               this.startTimer();
+              this.autoDraftOnClick();
             }}
           >
             start
           </button>
         ) : null}
+
         <Logger draftedPlayer={this.props.draftedPlayer} />
       </div>
     );
