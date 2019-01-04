71399f51cbc9aa1693362b53dd2f2257e10f8820
Frank Martinez <frankie.martinez@gmail.com>
2019-01-03 19:49:36 -0800

    Small style changes to draft page, player card

--- mox-drafting/src/components/DraftComponent/PlayerCard.js
+++ mox-drafting/src/components/DraftComponent/PlayerCard.js
@@ -11,37 +11,42 @@ const PlayerCard = props => {
     <div className="player-card d-flex ">
       <section className="container-fluid">
         <div className="row">
-          <div className="col-6  bg-dark player-stats">player stats</div>
-          <div className="col-6  bg-muted team-logo">team logo</div>
+          <div className="col-6  bg-dark   player-stats">player stats</div>
+          <div className="col-6  bg-dkgray   team-logo">team logo</div>
         </div>
 
-        <div className="d-flex  justify-content-around  align-items-end  w-100  mb-2">
+        
           {props.selection.length === 1 ? (
-            <div className="d-flex align-items-end w-100">
-              <div className="d-flex justify-content-around w-100 bg-dark">
-                <div>{selectedPlayer[0].overallRank} </div>
-                <div>{selectedPlayer[0].displayName} </div>
-                <div>{selectedPlayer[0].position} </div>
-              </div>
-
+            <div className="d-flex  justify-content-around  align-items-end   player-info">
+              <div className="d-flex align-items-end w-100">
               <div
-                className="btn btn-sm btn-primary  w-50"
-                onClick={() => {
-                  props.pickSelectedHandler();
-                  props.draftPlayer();
-                }}
-              >
-                DRAFT
+                  className="btn btn-sm btn-primary  w-100"
+                  onClick={() => {
+                    props.pickSelectedHandler();
+                    props.draftPlayer();
+                  }}
+                >
+                  DRAFT
+                </div>
+                <div className="d-flex justify-content-around w-100 bg-black">
+                  <div>{selectedPlayer[0].overallRank} </div>
+                  <div>{selectedPlayer[0].displayName} </div>
+                  <div>{selectedPlayer[0].position} </div>
+                </div>
+
+                
               </div>
             </div>
           ) : (
-            <div className="d-flex  w-100">
-              <h4 className="text-uppercase  text-center  bg-dark  w-100">
-                Select 1 player
-              </h4>
+            <div className="d-flex  justify-content-around  align-items-end   player-info ">
+              <div className="d-flex  w-100">
+                <h4 className="text-uppercase  text-center  bg-black  w-100">
+                  Select 1 player
+                </h4>
+              </div>
             </div>
           )}
-        </div>
+
       </section>
     </div>
   );
